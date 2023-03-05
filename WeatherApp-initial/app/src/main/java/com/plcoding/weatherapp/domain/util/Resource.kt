ee9726719d09f59ed1af921f4993fec107f3e853
Lisa Glaziou <lisa.glaziou@epitech.eu>
2023-03-05 17:05:55 +0100

    add: repository and implementation between the data and the domain

--- WeatherApp-initial/app/src/main/java/com/plcoding/weatherapp/domain/util/Resource.kt
+++ WeatherApp-initial/app/src/main/java/com/plcoding/weatherapp/domain/util/Resource.kt
@@ -1,5 +1,6 @@
 package com.plcoding.weatherapp.domain.util
 
+//class that determines the output of the api call
 sealed class Resource<T>(val data: T? = null, val message: String? = null) {
     class Success<T>(data: T?): Resource<T>(data)
     class Error<T>(message: String, data: T? = null): Resource<T>(data, message)
